49c5dcab5ca48b1cef32978661e813ae55bc43ca
Jon <chrono803@yahoo.com>
2012-11-29 22:17:49 -0500

    Small test to get the Python script working in C#. May be deleted later

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -1,14 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Assignment1
+﻿namespace Assignment1
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Dynamic;
+    using IronPython.Hosting;
+    using Microsoft.Scripting.Hosting;
+
     class Program
     {
         static void Main(string[] args)
         {
+            var engine = Python.CreateEngine();
+            var scope = engine.CreateScope();
+            var scriptSource = engine.CreateScriptSourceFromFile(@"C:\Users\Jon-Standard\Documents\Visual Studio 2010\Projects\LearnToProgramAssignments\Assignment1\test.py");
+
+            var compiled = scriptSource.Compile();
+
+            compiled.Execute(scope);
+
+            dynamic f_Add = scope.GetVariable("Add");
+
+            dynamic results = engine.Operations.Invoke(f_Add, 2, 2);
+
+            Console.WriteLine(results);
+
+            Console.ReadLine();
         }
     }
 }
---
+++ readme.txt
@@ -0,0 +1,3 @@
+Assignments from the Learn to Program course on Coursera - https://class.coursera.org/programming1-2012-001/
+
+Also, this is a good introduction to Python, IronPython, and using the dynamic keyword in C# to interact with the DLR.
