8f717caee7fca847a93fa55335cf46786617779e
Jon <chrono803@yahoo.com>
2012-12-01 21:49:47 -0500

    Added new tests

--- Assignment1/TimeFunctions.cs
+++ Assignment1/TimeFunctions.cs
@@ -7,12 +7,16 @@ namespace Assignment1
 {
     using System;
     using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
 
     /// <summary>
     /// 
     /// </summary>
     public class TimeFunctions
     {
+        ScriptSource scriptSource;
+        CompiledCode compiledCode;
+
         public TimeFunctions ()
 	    {}
 
@@ -23,19 +27,38 @@ namespace Assignment1
 
         public string ScriptPath { get; set; }
 
-        public dynamic GetSecondsDifference(double time1, double time2)
+        public double GetSecondsDifference(double time1, double time2)
         {
-            var engine = Python.CreateEngine();
+            var engine = CreatePythonEngineWithSourceFile();
+
             var scope = engine.CreateScope();
-            var scriptSource = engine.CreateScriptSourceFromFile(this.ScriptPath);
 
-            var compiled = scriptSource.Compile();
-
-            compiled.Execute(scope);
+            compiledCode.Execute(scope);
 
             dynamic f_Seconds_Difference = scope.GetVariable("seconds_difference");
 
-            return engine.Operations.Invoke(f_Seconds_Difference, time1, time2);
+            return (double)engine.Operations.Invoke(f_Seconds_Difference, time1, time2);
+        }
+
+        public double GetHoursDifference(double time1, double time2)
+        {
+            var engine = CreatePythonEngineWithSourceFile();
+
+            var scope = engine.CreateScope();
+
+            compiledCode.Execute(scope);
+
+            dynamic f_Hours_Difference = scope.GetVariable("hours_difference");
+
+            return (double)engine.Operations.Invoke(f_Hours_Difference, time1, time2);
+        }
+
+        private ScriptEngine CreatePythonEngineWithSourceFile()
+        {
+            var engine = Python.CreateEngine();
+            scriptSource = engine.CreateScriptSourceFromFile(this.ScriptPath);
+            compiledCode = scriptSource.Compile();
+            return engine;
         }
     }
 }
--- LearnToProgramAssignments.Tests/TimeFunctionsTests.cs
+++ LearnToProgramAssignments.Tests/TimeFunctionsTests.cs
@@ -6,16 +6,24 @@
     [TestFixture]
     public class TimeFunctionsTests
     {
-        private TimeFunctions timeFunctions = 
-            new TimeFunctions("TimeFunctions.py");
+        private TimeFunctions timeFunctions = new TimeFunctions("TimeFunctions.py");
 
         [TestCase(1800.0, 1800.0, 0.0)]
         [TestCase(1800.0, 3600.0, 1800.0)]
         [TestCase(1800.0, 2160.0, 360.0)]
         [TestCase(3600.0, 1800.0, -1800.0)]
-        public void GetSecondsDifference_Test(double time1, double time2, dynamic result)
+        public void GetSecondsDifference_Test(double time1, double time2, double result)
         {
-            Assert.IsTrue(timeFunctions.GetSecondsDifference(time1, time2) == (double)result);
+            Assert.IsTrue(timeFunctions.GetSecondsDifference(time1, time2) == result);
+        }
+
+        [TestCase(1800.0, 1800.0, 0.0)]
+        [TestCase(1800.0, 3600.0, 0.5)]
+        [TestCase(1800.0, 2160.0, 0.1)]
+        [TestCase(3600.0, 1800.0, -0.5)]
+        public void GetHoursDifference(double time1, double time2, double result)
+        {
+            Assert.IsTrue(timeFunctions.GetHoursDifference(time1, time2) == result);
         }
     }
 }
